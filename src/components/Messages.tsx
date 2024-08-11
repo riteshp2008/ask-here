520a1f44bcd6042bdbae59efe6846412613c2644
Ritesh <riteshpatel246@gmail.com>
2024-08-11 18:30:51 +0530

    vercel error

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -22,7 +22,7 @@ export const Messages = ({ messages }: MessagesProps) => {
           <MessageSquare className="size-8 text-blue-500" />
           <h3 className="font-semibold text-xl text-white">You are all set!</h3>
           <p className="text-zinc-500 text-sm">
-            Ask your first question to get started.
+            Ask your first question to get started
           </p>
         </div>
       )}
