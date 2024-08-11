9a6084d0ad7ed92b129c437fce22c69d1675d7e7
Ritesh <riteshpatel246@gmail.com>
2024-08-11 18:00:05 +0530

    chat bot created

---
+++ src/components/ChatInput.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { Button, Textarea } from "@nextui-org/react";
+import { Send } from "lucide-react";
+import { type useChat } from "ai/react";
+import { root } from "postcss";
+
+type HandleInputChange = ReturnType<typeof useChat>["handleInputChange"];
+type HandleSubmit = ReturnType<typeof useChat>["handleSubmit"];
+type SetInput = ReturnType<typeof useChat>["setInput"];
+
+interface ChatInputProps {
+  input: string;
+  handleInputChange: HandleInputChange;
+  handleSubmit: HandleSubmit;
+  setInput: SetInput;
+}
+
+export const ChatInput = ({
+  handleInputChange,
+  handleSubmit,
+  input,
+  setInput,
+}: ChatInputProps) => {
+  return (
+    <div className="z-10 bg-zinc-900 absolute bottom-0 left-0 w-full">
+      <div className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+        <div className="relative flex h-full flex-1 items-stretch md:flex-col">
+          <div className="relative flex flex-col w-full flex-grow p-4">
+            <form onSubmit={handleSubmit} className="relative">
+              <Textarea
+                minRows={3}
+                onChange={handleInputChange}
+                value={input}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" && !e.shiftKey) {
+                    e.preventDefault();
+                    handleSubmit();
+                    setInput("");
+                  }
+                }}
+                placeholder="Enter your question..."
+                classNames={{
+                  input:
+                    "pl-3 pt-2 border-0 resize-none bg-zinc-800 rounded-2xl text-base",
+                }}
+              />
+
+              <Button
+                size="sm"
+                type="submit"
+                className="absolute z-10 border bg-zinc-900 p-1 rounded-md right-2 bottom-2"
+              >
+                <Send className="size-4" />
+              </Button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
